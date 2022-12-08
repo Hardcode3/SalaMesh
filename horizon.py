fdd91594cbff4253b028fd7be0f066a8fcda17e7
Lucie Bertaud <bertaud4u@etu.univ-lorraine.fr>
2022-12-08 16:25:46 +0100

    extend the matrix to add laplace

--- horizon.py
+++ horizon.py
@@ -21,36 +21,49 @@ for c in range(m.ncorners): # count the number of horizon corner
         dico_corner_horizon[id].append(c)
 
 ids_horizon = dico_corner_horizon.keys()
-A = scipy.sparse.lil_matrix(( len(list_corner_horizon) + len(ids_horizon), m.nverts))
+A = scipy.sparse.lil_matrix(( len(list_corner_horizon) + len(ids_horizon) + m.ncorners, m.nverts))
 b = [0] * A.shape[0]
 
 row = 0
+
+#laplace
+for row in range(m.ncorners):
+    i = m.org(row)
+    j = m.dst(row)
+    A[row, j] =  1
+    A[row, i] = -1
+
+
+# Horizon
 list_i = []
 for id_hor in ids_horizon:
+    i =  m.dst(dico_corner_horizon[id_hor][-1])
+    y_hor = m.V[i][1] 
+    list_i.append(i)
+    A[row,i] =  1 *10
+    b[row] = y_hor *10
+
+    row += 1
     for corner in dico_corner_horizon[id_hor]:
         i = m.org(corner)
         list_i.append(i)
-        A[row, i] =  1
-        b[row] = id_hor
+        A[row, i] =  1 * 10
+        b[row] = y_hor * 10
         row += 1
-    i =  m.dst(dico_corner_horizon[id_hor][-1])
-    list_i.append(i)
-    A[row,i] =  1
-    b[row] = id_hor
-    row += 1
+    
 A = A.tocsr() # convert to compressed sparse row format for faster matrix-vector muliplications
 x = lsmr(A, b)[0] # call the least squares solver
 for i in list_i: # apply the computed flattening
         m.V[i][1] = x[i]
 
-for c in range(m.ncorners): # lift all vertices of all horizons
+""" for c in range(m.ncorners): # lift all vertices of all horizons
     if attr.horizon_id[c]>=0:
         height = (1+attr.horizon_id[c]) # arbitrarily chosen coeff to get a visually nice result
-        m.V[m.org(c)][2] = m.V[m.dst(c)][2] = height
+        m.V[m.org(c)][2] = m.V[m.dst(c)][2] = height """
 
 
 
 print(A)
 print(b)
 print(x)
-m.save("outout2.obj")
+m.save("evolution/2_horizon_laplace.obj")
