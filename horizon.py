de05f96b3172e35d460bfabb5596f4279ec4ad01
Lucie Bertaud <bertaud4u@etu.univ-lorraine.fr>
2022-12-09 08:39:14 +0100

    horizonatl and good mesh

--- horizon.py
+++ horizon.py
@@ -20,41 +20,48 @@ for c in range(m.ncorners): # count the number of horizon corner
             dico_corner_horizon[id] = []
         dico_corner_horizon[id].append(c)
 
+
 ids_horizon = dico_corner_horizon.keys()
-A = scipy.sparse.lil_matrix(( len(list_corner_horizon) + len(ids_horizon) + m.ncorners, m.nverts))
-b = [0] * A.shape[0]
 
-row = 0
+for _ in range(1):
+    A = scipy.sparse.lil_matrix(( len(list_corner_horizon) + len(ids_horizon) + m.ncorners + nboundary , m.nverts))
+    b = [0] * A.shape[0]
+    row = 0
 
-#laplace
-for row in range(m.ncorners):
-    i = m.org(row)
-    j = m.dst(row)
-    A[row, j] =  1
-    A[row, i] = -1
+    #laplace
+    for row in range(m.ncorners):
+        i = m.org(row)
+        j = m.dst(row)
+        A[row, j] =  1
+        A[row, i] = -1
+    for (i,v) in enumerate(m.V):
+        if m.on_border(i):
+            A[row, i] = 1 * 10 # quadratic penalty to lock boundary vertices
+            b[row] = v[1] * 10
+            row += 1
 
-
-# Horizon
-list_i = []
-for id_hor in ids_horizon:
-    i =  m.dst(dico_corner_horizon[id_hor][-1])
-    y_hor = m.V[i][1] 
-    list_i.append(i)
-    A[row,i] =  1 *10
-    b[row] = y_hor *10
-
-    row += 1
-    for corner in dico_corner_horizon[id_hor]:
-        i = m.org(corner)
+    # Horizon
+    list_i = []
+    for id_hor in ids_horizon:
+        i =  m.dst(dico_corner_horizon[id_hor][-1])
+        y_hor = m.V[i][1] 
         list_i.append(i)
-        A[row, i] =  1 * 10
-        b[row] = y_hor * 10
+        A[row,i] =  1 *10
+        b[row] = y_hor *10
+
         row += 1
-    
-A = A.tocsr() # convert to compressed sparse row format for faster matrix-vector muliplications
-x = lsmr(A, b)[0] # call the least squares solver
-for i in list_i: # apply the computed flattening
-        m.V[i][1] = x[i]
+        for corner in dico_corner_horizon[id_hor]:
+            i = m.org(corner)
+            list_i.append(i)
+            A[row, i] =  1 * 10
+            b[row] = y_hor * 10
+            row += 1
+        
+    A = A.tocsr() # convert to compressed sparse row format for faster matrix-vector muliplications
+    x = lsmr(A, b)[0] # call the least squares solver
+    for i in range(m.nverts): # apply the computed flattening
+            m.V[i][1] = x[i]
+
 
 """ for c in range(m.ncorners): # lift all vertices of all horizons
     if attr.horizon_id[c]>=0:
@@ -64,6 +71,4 @@ for i in list_i: # apply the computed flattening
 
 
 print(A)
-print(b)
-print(x)
-m.save("evolution/2_horizon_laplace.obj")
+m.save("evolution/4_Laplace_with_border.obj")
