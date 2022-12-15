659851abb6c3d386a2bc4f7910b5ef49b32cd99e
Lucie Bertaud <bertaud4u@etu.univ-lorraine.fr>
2022-12-15 10:34:55 +0100

    avec condition horizon par horizon

--- horizon.py
+++ horizon.py
@@ -10,23 +10,54 @@ m = Mesh(model+"/slice.obj")
 attr = importlib.import_module(model + ".attributes")
 #nboundary = sum(m.boundary)
 
-nhor = 0
-for c in range(m.ncorners): # count the number of horizon corner
+dico_corner_horizon = {}
+list_corner_horizon = []
+for c in range(m.ncorners): # pour decouper par horizon
     id = attr.horizon_id[c]
-    if id<0: continue
-    nhor += 1
+    if id>=0:
+        list_corner_horizon.append(c)
+        if id not in dico_corner_horizon:
+            dico_corner_horizon[id] = []
+        dico_corner_horizon[id].append(c)
 
-A = scipy.sparse.lil_matrix((m.ncorners+nhor+m.nverts, m.nverts))
+nhor = len(list_corner_horizon)
+ids_horizon = dico_corner_horizon.keys()
+
+
+################# Remplissage matrice#################################
+A = scipy.sparse.lil_matrix((m.ncorners+nhor + m.nverts, m.nverts))
 b = [0] * A.shape[0]
 
-for row in range(m.ncorners):
+for row in range(m.ncorners):  ###### beau maillage
     i = m.org(row)
     j = m.dst(row)
     A[row, j] =  1
     A[row, i] = -1
     b[row] = m.V[j][1] - m.V[i][1]
 
+cnt = 0
+for id_hor in range(len(ids_horizon)):  #####horizons
+    i_fix = m.org(dico_corner_horizon[id_hor][0])
+#    y_hor = m.V[i_fix][1] 
+#    print(id_hor, '    ', i_fix, '  ', y_hor)
+    for corner in dico_corner_horizon[id_hor]:
+        i = m.org(corner)
+        j = m.dst(corner)
+        if j==i_fix: continue
+        A[row, j] =  -100
+        A[row, i_fix] = 100
+        b[row] = 0
+        row += 1
+        cnt += 1
+"""       
+for v in range(m.nverts): #pour bloquer les composantes connexes
+    A[row, v] = 1*0.1
+    b[row] = m.V[v][1]*0.1
+    row += 1 """
 
+
+""" 
+PROF
 for c in range(m.ncorners): # count the number of horizon corner
     id = attr.horizon_id[c]
     if id<0: continue
@@ -34,12 +65,18 @@ for c in range(m.ncorners): # count the number of horizon corner
     j = m.dst(c)
     A[row, j] =  10
     A[row, i] = -10
-    row += 1
-
-for v in range(m.nverts):
+    row += 1 
+for v in range(m.nverts): #pour bloquer les composantes connexes
     A[row, v] = 1*0.1
     b[row] = m.V[v][1]*0.1
     row += 1
+
+    
+    """
+
+
+
+
 
 
 '''
@@ -147,14 +184,18 @@ for c in range(m.ncorners): # lift all vertices of all horizons
 A = A.tocsr() # convert to compressed sparse row format for faster matrix-vector muliplications
 x = lsmr(A, b)[0] # call the least squares solver
 for i in range(m.nverts): # apply the computed flattening
-        m.V[i][1] = x[i]
+    m.V[i][1] = x[i]
 
 for c in range(m.ncorners): # lift all vertices of all horizons
     if attr.horizon_id[c]>=0:
         height = (1+attr.horizon_id[c])/100. # arbitrarily chosen coeff to get a visually nice result
         m.V[m.org(c)][2] = m.V[m.dst(c)][2] = height
 
-print(A)
-print(b)
-print(x)
-m.save("evolution/8_fault_horizon.obj")
+for id_hor in range(len(ids_horizon)):  #####horizons
+    for i in range(10):
+        i_fix = m.org(dico_corner_horizon[id_hor][i])
+        y_hor = m.V[i_fix][1] 
+        print("    ", id_hor, '    ', i_fix, '  ', y_hor)
+        break
+
+m.save("evolution/9_fault_horizon.obj")
